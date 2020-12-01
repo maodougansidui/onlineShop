6786839336f3ea0f1457c2bae58cd221a24830fe
Jianlyu Mao <jianlyu@umail.ucsb.edu>
2020-11-30 16:23:54 -0800

    jm change the authority of super user

--- src/main/java/onlineShop/SecurityConfig.java
+++ src/main/java/onlineShop/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
-                .inMemoryAuthentication().withUser("stefanlaioffer@gmail.com").password("123").authorities("ROLE_ADMIN");
+                .inMemoryAuthentication().withUser("mao1246581855@gmail.com").password("123").authorities("ROLE_ADMIN");
 
         auth
                 .jdbcAuthentication()
